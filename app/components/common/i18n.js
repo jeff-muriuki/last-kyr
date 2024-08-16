439e7fb6bacfcb81ac7756dcf17089b72668e0d0
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-08-16 13:20:15 +0300

    es translation

--- app/components/common/i18n.js
+++ app/components/common/i18n.js
@@ -2,22 +2,22 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslation from './locales/en/translation.json';
 import esTranslation from './locales/es/translation.json';
-import esTranslation from './locales/fr/translation.json';
-import esTranslation from './locales/de/translation.json';
+import frTranslation from './locales/fr/translation.json';
+import deTranslation from './locales/de/translation.json';
 
 const resources = {
   en: { translation: enTranslation },
   es: { translation: esTranslation },
   fr: { translation: frTranslation },
   de: { translation: deTranslation },
-  
+
 };
 
 i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en', 
+    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
