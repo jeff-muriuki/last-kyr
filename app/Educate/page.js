61da0c42a027a1eb4eb671bcc0004ba3d4d46bd4
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-08-18 17:47:07 +0300

    saa kumi na moja na 47

---
+++ app/Educate/page.js
@@ -0,0 +1,29 @@
+"use client"
+
+import React from 'react';
+//import { Container, Row, Col, Card } from 'react-bootstrap';
+
+
+
+function Educate() {
+    return (
+        <Container className="my-5">
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    <Card className="shadow-lg">
+                        <Card.Body>
+                            <Card.Title className="text-center mb-4">
+                                Educate Yourself on Your Rights
+                            </Card.Title>
+                            <Card.Text>
+                                In the future, this section will include educational materials such as the constitution and other books to help you educate yourself more on matters pertaining to your rights. Stay tuned for more resources that will empower you with the knowledge to protect and exercise your rights.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Educate;
